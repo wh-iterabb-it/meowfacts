d22da00e3a2adcf1bd55023366e41469b1efd11c
beaubouchard <beau@beaubouchard.com>
2019-08-31 19:17:11 -0400

    chore: tiddy up

--- test/app.test.js
+++ test/app.test.js
@@ -1,31 +1,23 @@
-//During the test the env variable is set to test
-process.env.NODE_ENV = 'test';
-
-let app = require('../src/app');
-
-//Require the dev-dependencies
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-
-let should = chai.should();
-
+const app = require('../src/app');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
 
 chai.use(chaiHttp);
-//Our parent block
-describe('Main', () => {
 
-  /*
-  * Test the /GET route
-  */
+describe('Main', () => {
+  /**
+   * Test for root route, with GET request
+  **/
   describe('/GET facts', () => {
-      it('it should have successful GET', (done) => {
-        chai.request(app)
-            .get('/')
-            .end((err, res) => {
-              res.should.have.status(200);
-              done();
-            });
+    it('it should have successful GET', (done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
       });
+    });
   });
 
 });
