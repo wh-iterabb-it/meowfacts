da5efb3c851dfe7d8153e751b9f141f8254bb1ed
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2019-09-27 13:46:18 -0400

    test (#10)
    * chore: create .travis.yml
    
    * chore: create Dockerfile
    
    * chore: bootstrapped the app with a helloworld
    
    * chore: adding basic API with port 3001 hardcoded
    
    * chore: wrote basic server and test
    
    * chore: tiddy up
    
    * feat: changing port 80
    
    * test api endpoint
    
    * feat: changed port to dynamic
    
    * feat: port range
    
    * Update README.md
    
    * Update README.md
    
    * chore(package): update dependencies
    
    * chore(package): update lockfile package-lock.json
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * chore(package): update codecov to version 3.6.0
    
    * chore(package): update lockfile package-lock.json
    
    * fix(package): update helmet to version 3.21.1
    
    * chore(package): update lockfile package-lock.json
    
    * feat: health endpoint
    
    * feat: added uptime and version to health endpoint
    
    * Update .travis.yml
    
    * Update package.json

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM mhart/alpine-node:10.15.0
+
+# install git creating working directory
+RUN apk update && apk upgrade && \
+    apk add --no-cache bash git openssh && \
+    mkdir -p /usr/src/app
+
+# copy files to working directory
+COPY . /usr/src/app/
+
+# change working directory
+WORKDIR /usr/src/app
+
+# install node dependencies
+RUN npm install
+
+# expose server port
+EXPOSE 3000-5001
+
+# launch application
+CMD ["npm","start"]
---
+++ test/app.test.js
@@ -0,0 +1,23 @@
+const app = require('../src/app');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Main', () => {
+  /**
+   * Test for root route, with GET request
+  **/
+  describe('/GET facts', () => {
+    it('it should have successful GET', (done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+      });
+    });
+  });
+
+});
