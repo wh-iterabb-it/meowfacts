d269f2ec7a2a447854a337f23549ca632cc0c351
ifmycodecouldtalk <bonsoirelliot0101@gmail.com>
2022-10-10 23:26:14 -0400

    did some spanish translation, will do more later

--- src/models/localization/esp-MX.js
+++ src/models/localization/esp-MX.js
@@ -1,5 +1,9 @@
 const facts = [
   "En 1987, los gatos superaron a los perros como la mascota número uno en Estados Unidos.",
+  "Los gatos que viven juntos a veces se frotan la cabeza para demostrar que no tienen intención de pelear. Los gatos jóvenes hacen esto con más frecuencia, especialmente cuando están emocionados",
+  "Las mamás gatas enseñan a sus gatitos a usar la caja de arena.",
+  "Para darse de baja de catfacts, responda el siguiente código: tj3G5de$se",
+  "¡Tienes que ser gatito me! ¿Estás seguro de que quieres darte de baja? envía SÍ o NO",
 ];
 
 module.exports = {
