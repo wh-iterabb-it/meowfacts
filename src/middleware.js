18adafead0f84828e22c0885b60554c8b9433710
Youyou <JerryYang.XYY@outlook.com>
2024-07-06 00:46:11 +0800

    Revert "代码优化"
    This reverts commit 9edb1598b68e05dc8415010c4c2f4704856456e8.

--- src/middleware.js
+++ src/middleware.js
@@ -36,7 +36,10 @@ function validateCount(param, lang) {
  */
 function validateLanguage(language) {
   console.log(`checking ... ${language}`);
-  return VALID_LANGUAGES.includes(language);
+  if (VALID_LANGUAGES.indexOf(language) === -1) {
+    return false;
+  }
+  return true;
 }
 
 /**
@@ -46,11 +49,16 @@ function validateLanguage(language) {
  * @param {NextFunction} next - Express next function
  */
 function invalidLanguageMiddleware(request, response, next) {
-  if (request.query?.lang && !validateLanguage(request.query.lang)) {
-    response
-      .status(400)
-      .send(`Invalid language, valid languages are ${VALID_LANGUAGES.join(", ")}`);
-    return;
+  if (request.query && request.query.lang && request.query.lang != undefined) {
+    if (!validateLanguage(request.query.lang)) {
+      // language specified, so continue
+      response
+        .status(400)
+        .send(
+          `Invalid language, valid languages are ${VALID_LANGUAGES.join(", ")}`
+        );
+      return;
+    }
   }
   next();
 }
@@ -63,27 +71,37 @@ function invalidLanguageMiddleware(request, response, next) {
  * @param {NextFunction} next - Express next function
  */
 function invalidCountMiddleware(request, response, next) {
-  if (request.query?.count !== undefined) {
-    if (!validateCount(request.query.count, request.query.lang)) {
-      response
-        .status(400)
-        .send(`Invalid count, valid counts are between 2 and ${facts.getLanguageFacts(request.query.lang).length}`);
-      return;
-    }
+  if (
+    request.query &&
+    request.query.count &&
+    request.query.count == undefined
+  ) {
+    response
+      .status(400)
+      .send(
+        `Invalid count, valid counts are between 2 and ${
+          facts.getLanguageFacts(request.query.lang).length
+        }`
+      );
+    return;
   }
   next();
 }
 
 function invalidIDMiddleware(request, response, next) {
-  if (!request.query?.id) {
+  if (!request.query || !request.query.id || request.query.id.length != 0) {
     next(); // no id specified, so continue
-    return;
   }
 
   if (!validateCount(request.query.id, request.query.lang)) {
+    // id specified, so continue
     response
       .status(400)
-      .send(`Invalid ID, valid IDs are between 1 and ${facts.getLanguageFacts(request.query.lang).length}`);
+      .send(
+        `Invalid ID, valid IDs are between 1 and ${
+          facts.getLanguageFacts(request.query.lang).length
+        }`
+      );
     return;
   }
   next();
