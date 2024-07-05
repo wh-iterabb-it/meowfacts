26f3c73686f2dcf0e7ca05af573e5e01991d2c67
Youyou <JerryYang.XYY@outlook.com>
2024-07-06 00:45:12 +0800

    Revert "Update facts.js"
    This reverts commit 24702ae0bc59b04705fcfeffc70807a246162f30.

--- src/models/facts.js
+++ src/models/facts.js
@@ -3,55 +3,65 @@ const { localizations, defaultLanguages } = require("./localizations");
 /**
  * getLanguageFacts
  * @param {String} requestedLang - ISO 639-2 Code
- * @returns {Array} facts localized to the language
+ * @returns {String} fact localized to the language
  */
-const getLanguageFacts = (requestedLang) => {
-  if (defaultLanguages[requestedLang]) {
+function getLanguageFacts(requestedLang) {
+  if (requestedLang in defaultLanguages) {
     return defaultLanguages[requestedLang].facts;
   }
 
-  const language = Object.values(localizations).find(
-    ({ code, langISO }) => code === requestedLang || langISO === requestedLang
-  );
+  for (const language in localizations) {
+    if (
+      localizations[language].code === requestedLang ||
+      localizations[language].langISO === requestedLang
+    ) {
+      return localizations[language].facts;
+    }
+  }
 
-  return language ? language.facts : localizations["eng-us"].facts;
-};
+  return localizations["eng-us"].facts;
+}
 
 /**
  * getSingle
- * @description A simple function to return a random fact
+ * @description A simple function to return a random catfact
  * @param {Number} ID - the specific index / unique id of a fact starting at 1
- * @param {String} lang - language code
- * @returns {String} - a single fact
+ * @param {String} lang
+ * @returns {String} - a single fact about cats
  **/
-const getSingle = (ID = null, lang = "eng-us") => {
+function getSingle(ID = null, lang = "eng-us") {
   const facts = getLanguageFacts(lang);
-  return ID ? facts[ID - 1] : facts[Math.floor(Math.random() * facts.length)];
-};
+  if (ID) {
+    const id = ID - 1;
+    return facts[id];
+  }
+
+  return facts[Math.floor(Math.random() * facts.length)];
+}
 
 /**
  * getMany
- * @description A simple function to return multiple random facts
- * @param {Number} count - the number of facts to return
- * @param {String} lang - language code
- * @returns {Array[String]} - an array of facts
+ * @description A simple function to return multiple random catfacts
+ * @param {Number} count is a required param the number of catfacts to return in an array
+ * @param {String} lang
+ * @returns {Array[String]} many facts returned
  **/
-const getMany = (count, lang) => {
+function getMany(count, lang) {
   const facts = getLanguageFacts(lang);
-  return Array.from({ length: facts.length }, (_, i) => i)
+  return [...Array(facts.length).keys()]
     .sort(() => 0.5 - Math.random())
     .slice(0, count)
     .map((index) => facts[index]);
-};
+}
 
 /**
  * getLanguages
- * @description A simple function to return an array of language objects
- * @returns {Array[Object]} - an array of language objects
+ * @description A simple function to return array of language objects
+ * @returns {Array[Object]} array of language objects returned
  **/
-const getLanguages = () =>
-  Object.values(localizations).map(
-    ({
+function getLanguages() {
+  return Object.values(localizations).reduce((carry, language) => {
+    const {
       langLocale,
       langISO,
       code,
@@ -59,15 +69,21 @@ const getLanguages = () =>
       langName,
       codeName,
       facts,
-    }) => ({
-      locale_code: langLocale,
-      iso_code: langISO,
-      full_code: code,
-      local_name: langLocaleName,
-      english_name: langName,
-      full_name: codeName,
-      fact_count: facts?.length || 0,
-    })
-  );
+    } = language;
+
+    return [
+      ...carry,
+      {
+        locale_code: langLocale,
+        iso_code: langISO,
+        full_code: code,
+        local_name: langLocaleName,
+        english_name: langName,
+        full_name: codeName,
+        fact_count: facts?.length || 0,
+      },
+    ];
+  }, []);
+}
 
 module.exports = { getSingle, getMany, getLanguageFacts, getLanguages };
